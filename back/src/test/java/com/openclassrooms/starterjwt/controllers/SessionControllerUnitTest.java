66574c62773ef9cf34f426590655e29cf8962ca5
David Atlan <david@davipro.fr>
2024-04-06 20:00:28 +0200

    Resolved issue #5.

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
@@ -1,9 +1,13 @@
 package com.openclassrooms.starterjwt.controllers;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,5 +58,42 @@ public class SessionControllerUnitTest {
                .andExpect(status().isOk()) // Vérifie que le statut de la réponse est OK
                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // Vérifie que le type de contenu est JSON
                .andExpect(jsonPath("$.id", is(1))); // Vérifie que l'ID de la session dans le corps de la réponse est 1
+    }
+
+    @Test // Indique que c'est une méthode de test
+    @WithMockUser // Simule un utilisateur authentifié
+    public void testFindAll_Unit() throws Exception {
+        // Arrange
+        List<Session> sessions = new ArrayList<>();
+        Session session1 = new Session();
+        session1.setId(1L);
+        sessions.add(session1);
+
+        Session session2 = new Session();
+        session2.setId(2L);
+        sessions.add(session2);
+
+        List<SessionDto> sessionDtos = sessions.stream()
+            .map(session -> {
+                SessionDto dto = new SessionDto();
+                dto.setId(session.getId());
+                return dto;
+            }).collect(Collectors.toList());
+
+        // Configure les mocks pour retourner des objets spécifiques
+        when(sessionService.findAll()).thenReturn(sessions);
+        when(sessionMapper.toDto(sessions)).thenReturn(sessionDtos);
+
+        // Act
+        mockMvc.perform(get("/api/session"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$", hasSize(2)))
+            .andExpect(jsonPath("$[0].id", is(1)))
+            .andExpect(jsonPath("$[1].id", is(2)));
+
+        // Assert
+        verify(sessionService, times(1)).findAll();
+        verify(sessionMapper, times(1)).toDto(sessions);
     }
 }
