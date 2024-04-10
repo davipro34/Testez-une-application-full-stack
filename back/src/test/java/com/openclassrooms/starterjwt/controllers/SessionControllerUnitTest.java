5cdb9c0131ef23477ec8a689af32f05d30df6b1c
David Atlan <david@davipro.fr>
2024-04-10 18:39:03 +0200

    Removed an unnecessary import.

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
