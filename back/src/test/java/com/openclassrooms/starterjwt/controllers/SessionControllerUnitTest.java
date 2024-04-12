ecbac7e984a86cc729c6d871b3b88c3ca2ad5739
David Atlan <david@davipro.fr>
2024-04-12 18:10:10 +0200

    Udating tests for SessionController.

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerUnitTest.java
@@ -4,11 +4,11 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -17,6 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -42,7 +44,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import com.openclassrooms.starterjwt.dto.SessionDto;
-import com.openclassrooms.starterjwt.exception.NotFoundException;
 import com.openclassrooms.starterjwt.mapper.SessionMapper;
 import com.openclassrooms.starterjwt.models.Session;
 import com.openclassrooms.starterjwt.models.Teacher;
@@ -112,22 +113,31 @@ public class SessionControllerUnitTest {
         // Arrange
         when(sessionService.getById(1L)).thenReturn(session); // Mocke le comportement de sessionService.getById
         when(sessionMapper.toDto(session)).thenReturn(sessionDto); // Mocke le comportement de sessionMapper.toDto
-        // Act & Assert
+        // Act
         mockMvc.perform(get("/api/session/{id}", 1L)) // Effectue une requête GET sur /api/session/{id}
-               .andExpect(status().isOk()) // Attend un statut 200 OK
+        // Assert
+                .andExpect(status().isOk()) // Attend un statut 200 OK
                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // Attend un contenu de type application/json
                .andExpect(jsonPath("$.id", is(1))); // Attend que l'ID de la réponse soit 1
     }
 
     @Test // Indique que c'est une méthode de test
-    @WithMockUser // Exécute le test avec un utilisateur mocké
-    public void testFindById_Unit_SessionNotFound() throws Exception {
+    public void testFindById_SessionNotFound() {
         // Arrange
-        Long id = 1L; // Définit l'ID de la session à chercher
-        when(sessionService.getById(id)).thenThrow(new NotFoundException()); // Mocke le comportement de sessionService.getById pour qu'il lance une exception NotFoundException
-        // Act & Assert
-        mockMvc.perform(get("/api/session/{id}", id)) // Effectue une requête GET sur /api/session/{id}
-            .andExpect(status().isNotFound()); // Attend un statut 404 Not Found
+        when(sessionService.getById(anyLong())).thenReturn(null); // Simuler le comportement de sessionService.getById pour qu'il renvoie null, ce qui signifie que la session n'existe pas
+        // Act
+        ResponseEntity<?> response = sessionController.findById("1"); // Appeler la méthode findById avec un ID valide
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode()); // Vérifier que le statut de la réponse est NOT_FOUND
+    }
+    
+    @Test // Indique que c'est une méthode de test
+    public void testFindById_NumberFormatException() {
+        // Arrange // Pas de configuration nécessaire pour ce test
+        // Act
+        ResponseEntity<?> response = sessionController.findById("notANumber"); // Appeler la méthode findById avec un ID invalide (qui n'est pas un nombre)
+        // Assert
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()); // Vérifier que le statut de la réponse est BAD_REQUEST
     }
 
     @Test // Indique que c'est une méthode de test
@@ -162,18 +172,17 @@ public class SessionControllerUnitTest {
         verify(sessionMapper, times(1)).toDto(sessions); // Vérifie que sessionMapper.toDto a été appelé une fois
     }
 
-    // Teste la création d'une session
-    @Test
+    @Test // Teste la création d'une session
     @WithMockUser
     public void testCreate_Unit() throws Exception {
         initializeSession(); // Initialise la session
         initializeDto(); // Initialise le DTO de la session
-
+        // Arrange
         // Mocke le comportement des méthodes toEntity, create et toDto
         when(sessionMapper.toEntity(sessionDto)).thenReturn(session);
         when(sessionService.create(session)).thenReturn(session);
         when(sessionMapper.toDto(session)).thenReturn(sessionDto);
-
+        // Act
         // Effectue une requête POST et vérifie la réponse
         mockMvc.perform(post("/api/session")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -181,16 +190,16 @@ public class SessionControllerUnitTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(1)));
-
+        // Assert
         // Vérifie que les méthodes create et toDto ont été appelées une fois
         verify(sessionService, times(1)).create(any(Session.class));
         verify(sessionMapper, times(1)).toDto(any(Session.class));
     }
 
-    // Teste la mise à jour d'une session avec succès
-    @Test
+    @Test // Teste la mise à jour d'une session avec succès
     @WithMockUser
     public void testUpdate_Unit_Success() throws Exception {
+        // Arrange
         initializeSession(); // Initialise la session
         initializeDto(); // Initialise le DTO de la session
 
@@ -198,10 +207,10 @@ public class SessionControllerUnitTest {
         when(sessionService.update(anyLong(), any(Session.class))).thenReturn(session);
         when(sessionMapper.toEntity(any(SessionDto.class))).thenReturn(session);
         when(sessionMapper.toDto(any(Session.class))).thenReturn(sessionDto);
-
+        // Act
         // Effectue une mise à jour et vérifie la réponse
         ResponseEntity<?> response = sessionController.update("1", sessionDto);
-
+        // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode()); // Vérifie le statut de la réponse
         assertTrue(response.getBody() instanceof SessionDto); // Vérifie le type du corps de la réponse
         assertEquals(sessionDto, response.getBody()); // Vérifie le contenu du corps de la réponse
@@ -212,28 +221,15 @@ public class SessionControllerUnitTest {
         verify(sessionMapper, times(1)).toDto(any(Session.class));
     }
 
-    // Teste la mise à jour d'une session qui n'existe pas
-    @Test
-    @WithMockUser
-    public void testUpdate_Unit_SessionNotFound() throws Exception {
-        initializeDto(); // Initialise le DTO de la session
-
-        // Mocke le comportement des méthodes update, toEntity et toDto
-        when(sessionService.update(anyLong(), any(Session.class))).thenThrow(new NotFoundException());
-        when(sessionMapper.toEntity(any(SessionDto.class))).thenReturn(session);
-
-        // Effectue une mise à jour et vérifie la réponse
-        // Capture l'exception NotFoundException lors de l'appel à la méthode update du sessionController avec les arguments "1" et sessionDto
-        Exception exception = assertThrows(NotFoundException.class, () -> {
-            sessionController.update("1", sessionDto);
-        });
-
-        // Vérifie que l'exception est bien de type NotFoundException
-        assertTrue(exception instanceof NotFoundException);
-
-        // Vérifie que les méthodes update et toEntity ont été appelées une fois
-        verify(sessionService, times(1)).update(anyLong(), any(Session.class));
-        verify(sessionMapper, times(1)).toEntity(any(SessionDto.class));
+    @Test // Annoter la méthode avec @Test pour indiquer qu'il s'agit d'un test unitaire
+    public void testUpdate_SessionBadRequest() {
+        // Arrange
+        String id = "not_a_number"; // Définir un ID de session non numérique pour déclencher NumberFormatException
+        SessionDto sessionDto = new SessionDto(); // Créer un nouvel objet SessionDto
+        // Act
+        ResponseEntity<?> response = sessionController.update(id, sessionDto); // Appeler la méthode update sur le contrôleur avec l'ID de la session et l'objet sessionDto
+        // Assert
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()); // Vérifier que le statut de la réponse est 400 BAD_REQUEST
     }
 
     // Teste la suppression d'une session
@@ -250,21 +246,38 @@ public class SessionControllerUnitTest {
         verify(sessionService, times(1)).delete(Long.valueOf(id)); // Vérifier que la méthode delete de sessionService a été appelée une fois avec l'ID de la session
     }
 
-    // Teste la suppression d'une session si non trouvée
     @Test
-    public void testDelete_Unit_SessionNotFound() {
+    public void testSave_SessionNotFound() {
         // Arrange
-        String id = "1"; // Définir l'ID de la session à supprimer
-        when(sessionService.getById(Long.valueOf(id))).thenReturn(null); // Simuler le comportement de sessionService pour retourner null lorsqu'on cherche la session
+        // Simuler le comportement de sessionService.getById pour qu'il renvoie null, ce qui signifie que la session n'existe pas
+        when(sessionService.getById(anyLong())).thenReturn(null);
+    
         // Act
-        ResponseEntity<?> response = sessionController.save(id); // Appeler la méthode de suppression sur le contrôleur avec l'ID de la session
+        // Appeler la méthode save avec un ID valide
+        ResponseEntity<?> response = sessionController.save("1");
+    
         // Assert
-        assertThat(response.getStatusCode(), is(HttpStatus.NOT_FOUND)); // Vérifier que le statut de la réponse est 404 NOT FOUND
+        // Vérifier que le statut de la réponse est NOT_FOUND
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+    
+    @Test
+    public void testSave_InvalidId() {
+        // Arrange
+        // Pas de configuration nécessaire pour ce test
+    
+        // Act
+        // Appeler la méthode save avec un ID invalide (qui n'est pas un nombre)
+        ResponseEntity<?> response = sessionController.save("invalid");
+    
+        // Assert
+        // Vérifier que le statut de la réponse est BAD_REQUEST
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
     // Teste la participation d'un utilisateur à une session
     @Test
-    public void testParticipate_Unit() {
+    public void testParticipate() {
         // Arrange
         String sessionId = "1"; // Définir l'ID de la session
         String userId = "2"; // Définir l'ID de l'utilisateur
@@ -277,6 +290,31 @@ public class SessionControllerUnitTest {
         verify(sessionService, times(1)).participate(Long.valueOf(sessionId), Long.valueOf(userId)); // Vérifier que la méthode participate du service SessionService a été appelée une fois avec les mêmes ID de session et d'utilisateur
         assertThat(response.getStatusCode(), is(HttpStatus.OK)); // Vérifier que le statut de la réponse est 200 OK
     }
+
+    @Test
+    public void testParticipate_BadRequest() {
+        // Arrange
+        // On définit des valeurs pour id et userId qui ne sont pas des nombres valides
+        String id = "notANumber";
+        String userId = "1";
+
+        // On crée une instance de SessionController avec les services mockés
+        SessionController controller = new SessionController(sessionService, sessionMapper);
+
+        // On configure le service mocké pour lever une NumberFormatException
+        // quand la méthode participate est appelée avec n'importe quel long
+        doThrow(NumberFormatException.class).when(sessionService).participate(anyLong(), anyLong());
+
+        // Act
+        // On appelle la méthode participate du contrôleur avec les valeurs définies plus haut
+        ResponseEntity<?> response = controller.participate(id, userId);
+
+        // Assert
+        // On vérifie que le statut de la réponse est BAD_REQUEST
+        // Ce qui signifie que le contrôleur a bien géré l'exception et renvoyé la bonne réponse
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+    
     // Teste le renoncement de participation d'un utilisateur à une session
     @Test
     public void testNoLongerParticipate() {
@@ -292,4 +330,21 @@ public class SessionControllerUnitTest {
         verify(sessionService, times(1)).noLongerParticipate(Long.valueOf(sessionId), Long.valueOf(userId)); // Vérifier que la méthode noLongerParticipate du service SessionService a été appelée une fois avec les mêmes ID de session et d'utilisateur
         assertThat(response.getStatusCode(), is(HttpStatus.OK)); // Vérifier que le statut de la réponse est 200 OK
     }
+
+    @Test
+    public void testNoLongerParticipate_BadRequest() {
+        // Arrange
+        String sessionId = "abc"; // Définir un ID de session non numérique pour déclencher NumberFormatException
+        String userId = "123"; // Définir un ID d'utilisateur quelconque
+    
+        // Simuler le comportement de sessionService pour lancer une NumberFormatException lorsqu'on appelle la méthode noLongerParticipate
+        doThrow(NumberFormatException.class).when(sessionService).noLongerParticipate(anyLong(), anyLong());
+    
+        // Act
+        ResponseEntity<?> response = sessionController.noLongerParticipate(sessionId, userId); // Appeler la méthode noLongerParticipate sur le contrôleur avec l'ID de la session et l'ID de l'utilisateur
+    
+        // Assert
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()); // Vérifier que le statut de la réponse est 400 BAD_REQUEST
+    }
+
 }
