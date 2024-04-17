915f26e39a795e6c3b99e189c33da441dffa93f6
David Atlan <david@davipro.fr>
2024-04-17 12:19:44 +0200

    Replaced the initial AppComponent integration test.

--- front/src/app/app.component.integration.spec.ts
+++ front/src/app/app.component.integration.spec.ts
@@ -2,15 +2,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { expect } from '@jest/globals';
+import { SessionService } from 'src/app/services/session.service';
+import { NgZone } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let service: SessionService;
+  let ngZone: NgZone;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule.withRoutes([
+          { path: '', component: AppComponent}
+        ]),
         RouterTestingModule,
         HttpClientModule,
         MatToolbarModule
@@ -19,11 +30,12 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
 });
