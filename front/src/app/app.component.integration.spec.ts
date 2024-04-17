a888ca360b391aa8a68d36cdaaaad1f05c10ddd2
David Atlan <david@davipro.fr>
2024-04-17 14:38:42 +0200

    In AppComponent, testing the logout() function and its interaction with SessionService and Router.

--- front/src/app/app.component.integration.spec.ts
+++ front/src/app/app.component.integration.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 import { of } from 'rxjs';
@@ -61,6 +62,32 @@ describe('AppComponent', () => {
 
     // Vérification que la fonction $isLogged() de AppComponent retourne le bon résultat
     expect(observerSpy.getLastValue()).toBe(true);
+  });
+
+  // Test de la fonction logout() et de son interaction avec SessionService et Router.
+  it('should log out the user and navigate to the root', () => {
+    // Given
+    // Création d'un spy sur la fonction logOut() de SessionService
+    // et simulation de son comportement pour qu'elle ne fasse rien (puisqu'elle ne retourne rien)
+    jest.spyOn(service, 'logOut').mockImplementation(() => {});
+
+    // Récupération de l'instance de Router injectée dans le TestBed
+    const router = TestBed.inject(Router);
+
+    // Création d'un spy sur la fonction navigate() de Router
+    // et simulation de son comportement pour qu'elle ne fasse rien (puisqu'elle retourne un Promise)
+    jest.spyOn(router, 'navigate').mockImplementation(() => Promise.resolve(true));
+
+    // When
+    // Appel de la fonction logout() de AppComponent
+    app.logout();
+
+    // Then
+    // Vérification que la fonction logOut() de SessionService a été appelée
+    expect(service.logOut).toHaveBeenCalled();
+
+    // Vérification que la fonction navigate() de Router a été appelée avec le bon argument
+    expect(router.navigate).toHaveBeenCalledWith(['']);
   });
 
 });
