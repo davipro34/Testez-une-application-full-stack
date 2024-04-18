4e8d0d82d31293e332bae6f7deeb204b1071d9b5
David Atlan <david@davipro.fr>
2024-04-18 11:31:20 +0200

    Adding a unit test for logIn() of SessionService.

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { ObserverSpy } from '@hirez_io/observer-spy';
 
 import { SessionService } from './session.service';
-import { ObserverSpy } from '@hirez_io/observer-spy';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -16,20 +17,46 @@ describe('SessionService', () => {
     expect(service).toBeTruthy();
   });
 
-    // Test de la méthode $isLogged() pour vérifier qu'elle retourne un Observable qui émet la valeur actuelle de isLogged.
-    describe('$isLogged', () => {
-      // Test pour vérifier que la méthode $isLogged retourne un Observable qui émet la valeur actuelle de isLogged
-      it('should return an Observable that emits the current value of isLogged', () => {
-        // Given
-        const observerSpy = new ObserverSpy(); // Création d'un espion d'observateur pour espionner l'Observable retourné par la méthode $isLogged
-        service.isLogged = true; // Définition de la valeur de isLogged
-  
-        // When
-        service.$isLogged().subscribe(observerSpy); // Abonnement à l'Observable retourné par la méthode $isLogged et espionnage de cet Observable
-  
-        // Then
-        expect(observerSpy.receivedNext()).toBe(true); // Vérification que l'Observable a émis la valeur actuelle de isLogged
-      });
-    });
+  // Test de la méthode $isLogged() pour vérifier qu'elle retourne un Observable qui émet la valeur actuelle de isLogged.
+  describe('$isLogged', () => {
+    // Test pour vérifier que la méthode $isLogged retourne un Observable qui émet la valeur actuelle de isLogged
+    it('should return an Observable that emits the current value of isLogged', () => {
+      // Given
+      const observerSpy = new ObserverSpy(); // Création d'un espion d'observateur pour espionner l'Observable retourné par la méthode $isLogged
+      service.isLogged = true; // Définition de la valeur de isLogged
 
+      // When
+      service.$isLogged().subscribe(observerSpy); // Abonnement à l'Observable retourné par la méthode $isLogged et espionnage de cet Observable
+
+      // Then
+      expect(observerSpy.receivedNext()).toBe(true); // Vérification que l'Observable a émis la valeur actuelle de isLogged
+    });
+  });
+
+  // Test de la méthode logIn
+  describe('logIn', () => {
+    // Test pour vérifier que la méthode logIn met à jour isLogged et sessionInformation correctement
+    it('should update isLogged to true and sessionInformation when logIn is called', () => {
+      // Given
+      const user: SessionInformation = {
+        token: 'testToken',
+        type: 'testType',
+        id: 1,
+        username: 'testUsername',
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        admin: false
+      };
+      const observerSpy = new ObserverSpy<boolean>(); // Création d'un espion d'observateur pour espionner l'Observable retourné par la méthode $isLogged
+      service.$isLogged().subscribe(observerSpy); // Abonnement à l'Observable retourné par la méthode $isLogged et espionnage de cet Observable
+
+      // When
+      service.logIn(user); // Appel de la méthode logIn avec l'utilisateur de test
+
+      // Then
+      expect(service.isLogged).toBe(true); // Vérification que isLogged est passé à true
+      expect(service.sessionInformation).toBe(user); // Vérification que sessionInformation a été mis à jour avec l'utilisateur de test
+      expect(observerSpy.getLastValue()).toBe(true); // Vérification que l'Observable a émis la valeur true
+    });
+  });
 });
