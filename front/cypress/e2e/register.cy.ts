a1a885c1f5e7f34d7318d15116409a90e134f4a1
David Atlan <david@davipro.fr>
2024-04-22 17:03:32 +0200

    In register: added e2e test "should register, log in, verify user account details and delete it".

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -30,5 +30,77 @@ describe('Register spec', () => {
 
     // Vérification que le bouton de soumission est désactivé
     cy.get('button[type=submit]').should('be.disabled')
-  })
+  });
+
+
+  it('should register, log in, verify user account details and delete it', () => {
+    // Given
+    // Interception de la requête d'inscription et simulation d'une réponse réussie
+    cy.intercept('POST', '/api/auth/register', {
+      statusCode: 201,
+      body: user,
+    }).as('register')
+
+    // When
+    // Remplissage du formulaire d'inscription et soumission
+    cy.get('input[formControlName=firstName]').type(user.firstName)
+    cy.get('input[formControlName=lastName]').type(user.lastName)
+    cy.get('input[formControlName=email]').type(user.email)
+    cy.get('input[formControlName=password]').type(user.password)
+    cy.get('button[type=submit]').click()
+
+    // Then
+    // Vérification que l'URL a changé pour la page de connexion
+    cy.url().should('include', '/login')
+
+    // Given
+    // Interception de la requête de connexion et simulation d'une réponse réussie
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 201,
+      body: user,
+    }).as('login')
+
+    // Interception de la requête de session et simulation d'une réponse réussie
+    cy.intercept('GET', '/api/session', {
+      statusCode: 200,
+      body: user,
+    }).as('getSession')
+
+    // When
+    // Remplissage du formulaire de connexion et soumission
+    cy.get('input[formControlName=email]').type(user.email)
+    cy.get('input[formControlName=password]').type(`${user.password}{enter}{enter}`)
+
+    // Interception de la requête de l'utilisateur et simulation d'une réponse réussie
+    cy.intercept('GET', `/api/user/${user.id}`, {
+      statusCode: 200,
+      body: user,
+    }).as('getUser')
+
+    // Clic sur le lien vers la page de l'utilisateur
+    cy.get('span[routerLink=me]').click();
+
+    // Then
+    // Vérification que l'URL a changé pour la page de l'utilisateur
+    cy.url().should('include', '/me');
+
+    // Vérification que les détails de l'utilisateur sont corrects
+    cy.get('.m3 mat-card-content p').contains(`Name: ${user.firstName} ${user.lastName.toUpperCase()}`).should('exist');
+    cy.get('.m3 mat-card-content p').contains(`Email: ${user.email}`).should('exist');
+    cy.get('.m3 mat-card-content div.my2').should('exist');
+
+    // Given
+    // Interception de la requête DELETE et simulation d'une réponse réussie
+    cy.intercept('DELETE', `/api/user/${user.id}`, {
+      statusCode: 200,
+    }).as('deleteUser')
+
+    // When
+    // Clique sur bouton de suppression
+    cy.get('.my2 > .mat-focus-indicator').click();
+
+    // Then
+    // Vérification que la requête DELETE a été appelée
+    cy.wait('@deleteUser').its('response.statusCode').should('eq', 200);
+  });
 })
