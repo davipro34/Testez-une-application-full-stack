efcd1e66310e70375300ec158945c93387da32a7
David Atlan <david@davipro.fr>
2024-04-23 14:09:03 +0200

    Configured Cypress account for dashboard.

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "95v8nj",
   videosFolder: 'cypress/videos',
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
